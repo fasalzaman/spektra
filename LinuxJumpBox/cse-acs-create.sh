ef77279409c669ff63979f52ba48c8a883cffdfa
fasalzaman <fasal.zaman@gmail.com>
2017-10-23 22:10:31 +0530

    Add files via upload

--- LinuxJumpBox/cse-acs-create.sh
+++ LinuxJumpBox/cse-acs-create.sh
@@ -5,7 +5,7 @@ RG_NAME=${3}
 SP_NAME=${4}
 SP_SECRET=${5}
 VM_SIZE=${6}
-"\n"
+echo "\n"
 echo RG_NAME=$RG_NAME >> /home/$adminUsername/acs-create.sh
 echo ACS_NAME=$adminUsername >> /home/$adminUsername/acs-create.sh 
 echo SSH_KEY_PATH='~/.ssh/id_rsa.pub' >> /home/$adminUsername/acs-create.sh 
@@ -17,8 +17,7 @@ echo DNS_PREFIX=$adminUsername >> /home/$adminUsername/acs-create.sh
 echo VM_SIZE=$VM_SIZE >> /home/$adminUsername/acs-create.sh
 echo ADMIN_NAME=$adminUsername >> /home/$adminUsername/acs-create.sh
 echo TENANT_ID='12c5db39-b62e-4301-b848-09acda2692a5' >> /home/$adminUsername/acs-create.sh
-"\n"
-"\n"
+
 TENANT_ID='12c5db39-b62e-4301-b848-09acda2692a5'
 ACS_NAME=$adminUsername
 DNS_PREFIX=$adminUsername
@@ -26,8 +25,10 @@ ADMIN_NAME=$adminUsername
 SSH_KEY_PATH='~/.ssh/id_rsa.pub'
 AGENT_OS_DISK=120
 MASTER_OS_DISK=50
+
+echo "\n"
 echo az login --service-principal -u $SP_NAME -p $SP_SECRET --tenant $TENANT_ID >> /home/$adminUsername/acs-create.sh
-"\n"
+echo "\n"
 echo az acs create --orchestrator-type Kubernetes -g $RG_NAME -n $ACS_NAME  \
 --ssh-key-value $SSH_KEY_PATH --service-principal $SP_NAME --client-secret  \
 $SP_SECRET --dns-prefix $DNS_PREFIX --master-count 1 --master-vm-size \
